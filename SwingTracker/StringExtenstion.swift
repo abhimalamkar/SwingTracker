e22f7d80ac941856dd5d74e8ae541ee1367ad56f
Abhijeet Malamkar <abhijeetmalamkar@gmail.com>
2019-09-11 09:17:28 -0500

    first commit

---
+++ SwingTracker/StringExtenstion.swift
@@ -0,0 +1,9 @@
+//
+//  StringExtenstion.swift
+//  SwingTracker
+//
+//  Created by Abhijeet Malamkar on 9/10/19.
+//  Copyright © 2019 Abhijeet Malamkar. All rights reserved.
+//
+
+import Foundation
---
+++ SwingTracker/SwingTracker.swift
@@ -0,0 +1,28 @@
+//
+//  API.swift
+//  SwingTracker
+//
+//  Created by Abhijeet Malamkar on 9/10/19.
+//  Copyright © 2019 Abhijeet Malamkar. All rights reserved.
+//
+
+import Foundation
+
+
+enum Axis {
+    case x,y,z
+}
+
+struct DimentionalData {
+    //
+    var x:Double? = 0
+    var y:Double? = 0
+    var z:Double? = 0
+}
+
+class
+
+
+
+
+
